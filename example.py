a6530a55eb0e80c41c479c6fbad3cea145441c78
KevMcK <kmcrunk@gmail.com>
2024-10-22 16:59:32 -0500

    example created from link

---
+++ example.py
@@ -0,0 +1,168 @@
+
+
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+# setup --------------------------------------------------------------
+dataset_train_filepath = './data/train.csv'
+dataset_test_filepath = './data/train.csv'
+
+train_df = pd.read_csv(dataset_train_filepath)
+test_df = pd.read_csv(dataset_test_filepath)
+
+# data type and missing values of each column
+# train_df.info()
+# test_df.info()
+# train_df.describe()
+# test_df.describe()
+# train_df.head()
+# test_df.head()
+
+
+# visualize --------------------------------------------------------------
+# sns.displot(train_df['SalePrice'], kde=True)
+# plt.show()
+
+# plt.rcParams['figure.figsize'] = (15, 15)
+# numeric_train_df = train_df.select_dtypes(include=['float64', 'int64'])
+# corr_matrix = numeric_train_df.corr()
+# g = sns.heatmap(corr_matrix, annot=True, fmt=".1f")
+# plt.show()
+
+# sns.barplot(x='YearBuilt', y='SalePrice', data=train_df)
+# plt.show()
+
+# sns.barplot(x='SaleCondition', y='SalePrice', data=train_df)
+# plt.show()
+
+# sns.barplot(x='YrSold', y='SalePrice', data=train_df)
+# plt.show()
+
+# clean up --------------------------------------------------------------
+train_df=train_df.drop("Id",axis=1)
+train_df=train_df.drop("Alley",axis=1)
+train_df=train_df.drop("PoolQC",axis=1)
+train_df=train_df.drop("Fence",axis=1)
+train_df=train_df.drop("MiscFeature",axis=1)
+test_df=test_df.drop("Alley",axis=1)
+test_df=test_df.drop("PoolQC",axis=1)
+test_df=test_df.drop("Fence",axis=1)
+test_df=test_df.drop("MiscFeature",axis=1)
+
+train_df["LotFrontage"] = train_df["LotFrontage"].fillna(train_df["LotFrontage"].mean())
+train_df["MasVnrArea"] = train_df["MasVnrArea"].fillna(train_df["MasVnrArea"].mean())
+train_df["GarageYrBlt"] = train_df["GarageYrBlt"].fillna(2001)
+
+c = ("GarageType", "GarageFinish", "GarageQual", "GarageCond", "BsmtFinType2", "BsmtCond", "BsmtQual", "BsmtExposure", "MasVnrType", "Electrical", "FireplaceQu", "BsmtFinType1")
+for col in c:
+  if train_df[col].dtype == "object":
+    train_df[col] = train_df[col].fillna("None")
+
+''' OR
+for col in ("GarageType", "GarageFinish", "GarageQual", "GarageCond", "BsmtFinType2", "BsmtCond", "BsmtQual", "BsmtExposure", "MasVnrType", "Electrical", "FireplaceQu", "BsmtFinType1"):
+  test_df[col] = test_df[col].fillna('None')
+'''
+
+test_df["LotFrontage"] = test_df["LotFrontage"].fillna(test_df["LotFrontage"].mean())
+test_df["MasVnrArea"] = test_df["MasVnrArea"].fillna(test_df["MasVnrArea"].mean())
+test_df["GarageYrBlt"] = test_df["GarageYrBlt"].fillna(2001)
+test_df["GarageCars"] = test_df["GarageCars"].fillna(0)
+test_df["GarageArea"] = test_df["GarageArea"].fillna(test_df["GarageArea"].mean())
+test_df["BsmtFullBath"] = test_df["BsmtFullBath"].fillna(0)
+test_df["BsmtHalfBath"] = test_df["BsmtHalfBath"].fillna(0)
+test_df["BsmtFinSF1"] = test_df["BsmtFinSF1"].fillna(test_df["BsmtFinSF1"].mean())
+test_df["BsmtFinSF2"] = test_df["BsmtFinSF2"].fillna(test_df["BsmtFinSF2"].mean())
+test_df["TotalBsmtSF"] = test_df["TotalBsmtSF"].fillna(test_df["TotalBsmtSF"].mean())
+test_df["BsmtUnfSF"] = test_df["BsmtUnfSF"].fillna(test_df["BsmtUnfSF"].mean())
+
+c = ("GarageType", "GarageFinish", "GarageQual", "GarageCond", "BsmtFinType2", "BsmtCond", "BsmtQual", "BsmtExposure", "MasVnrType", "Electrical","MSZoning","Utilities","Exterior1st","Exterior2nd","KitchenQual","Functional","FireplaceQu","SaleType", "BsmtFinType1")
+for col in c:
+  if test_df[col].dtype == "object":
+    test_df[col] = test_df[col].fillna("None")
+
+''' OR
+for col in ("GarageType", "GarageFinish", "GarageQual", "GarageCond", "BsmtFinType2", "BsmtCond", "BsmtQual", "BsmtExposure", "MasVnrType", "Electrical","MSZoning","Utilities","Exterior1st","Exterior2nd","KitchenQual","Functional","FireplaceQu","SaleType", "BsmtFinType1"):
+  test_df[col] = test_df[col].fillna('None')
+'''
+
+# train_df.info()
+# test_df.info()
+
+# Encoding --------------------------------------------------------------
+
+catagory_cols = ('MSZoning','Street','LotShape','LandContour','Utilities','LotConfig','LandSlope','Neighborhood','Condition1','Condition2','BldgType', 'HouseStyle', 'RoofStyle','RoofMatl','Exterior1st','Exterior2nd','ExterCond','Foundation','Heating','HeatingQC','CentralAir','KitchenQual','Functional','FireplaceQu','PavedDrive','SaleType','SaleCondition', "GarageType", "GarageFinish", "GarageQual", "GarageCond", "BsmtFinType2", "BsmtCond", "BsmtQual", "BsmtExposure", "MasVnrType", "Electrical", "BsmtFinType1", "ExterQual")
+for c in catagory_cols:
+  le = LabelEncoder()
+  train_df[c]= le.fit_transform(train_df[c].values)
+
+
+catagory_cols = ('MSZoning','Street','LotShape','LandContour','Utilities','LotConfig','LandSlope','Neighborhood','Condition1','Condition2','BldgType', 'HouseStyle', 'RoofStyle','RoofMatl','Exterior1st','Exterior2nd','ExterCond','Foundation','Heating','HeatingQC','CentralAir','KitchenQual','Functional','FireplaceQu','PavedDrive','SaleType','SaleCondition', "GarageType", "GarageFinish", "GarageQual", "GarageCond", "BsmtFinType2", "BsmtCond", "BsmtQual", "BsmtExposure", "MasVnrType", "Electrical", "BsmtFinType1", "ExterQual")
+for c in catagory_cols:
+  le = LabelEncoder()
+  test_df[c]= le.fit_transform(test_df[c].values)
+
+# print(train_df.head())
+# print(test_df.head())
+
+
+# Modeling --------------------------------------------------------------
+# Drop target column 'SalePrice' for X_train
+X_train = train_df.drop("SalePrice", axis=1)
+Y_train = train_df["SalePrice"]
+
+# Drop 'Id' column from test data for X_test
+X_test  = test_df.drop("Id", axis=1).copy()
+
+# Ensure X_test columns match X_train
+X_test = X_test[X_train.columns]
+
+# Apply standard scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Output transformed data and target
+# print(X_train)
+# print(Y_train)
+
+# Principle Component Analysis
+
+pca = PCA(n_components = 10)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+
+
+
+regressor = RandomForestRegressor(n_estimators = 200, random_state = 0)
+regressor.fit(X_train, Y_train)
+Y_pred = regressor.predict(X_test)
+
+
+''' OR
+from xgboost import XGBRegressor
+regressor = XGBRegressor()
+regressor.fit(X_train, Y_train)
+Y_pred = regressor.predict(X_test)
+'''
+
+
+regressor.score(X_train, Y_train)
+regressor = round(regressor.score(X_train, Y_train) * 100, 2)
+print(regressor)
+
+# submission = pd.DataFrame({
+#         "Id": test_df["Id"],
+#         "SalePrice": Y_pred
+#     })
+#
+# submission.to_csv('RandomForest.csv', index=False)
+
